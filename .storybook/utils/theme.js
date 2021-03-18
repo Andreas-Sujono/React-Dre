5fea65f1c39ee8a2052d81e106a5493bc9db1f3a
AndreasSujono <andreas.sujono@dathena.io>
2021-03-18 18:08:03 +0800

    change storybook image link

--- .storybook/utils/theme.js
+++ .storybook/utils/theme.js
@@ -4,7 +4,7 @@ export const theme = create({
     base: 'light',
     brandTitle: 'React Dre',
     brandUrl: 'https://github.com/Andreas-Sujono/React-Dre',
-    brandImage: '/assets/logo-with-name.png',
+    brandImage: 'https://andreas-sujono.github.io/React-Dre/assets/logo-with-name.png',
     // fontBase: 'Lato',
     // colorPrimary: '#13153c',
     // colorSecondary: '#555670',
